e8cee5f92db6bb8150268d4de812ff2add49326b
Manuel Chingay <chingayloez4@gmail.com>
2026-04-01 01:00:03 -0400

    Tarea 3 ejercicio 2

---
+++ tarea_3_unad/2_altura.py
@@ -0,0 +1,8 @@
+# Solicitar altura
+
+altura = float(input('Ingrese su altura: '))
+
+if altura >= 1.20:
+    print('Puede ingresar a la montaña rusa')
+else:
+    print('No puede ingresar a la montaña rusa')
